90a688b6843d63cd2a4f07493ba50fda78aa8978
Diego Martinez <dmartinezgarcia@outlook.com>
2015-07-20 01:47:14 +0200

    Chapter 1 exercises

---
+++ Chapter 1 - Getting Started The Game Over Program/exercise_1.py
@@ -0,0 +1,7 @@
+# Exercise 1
+#
+# Create an error of your very own by entering your favorite ice cream flavor in interactive mode. Then, make up for
+# your misdeed and enter a statement that prints the name of your favorite ice cream.
+#
+
+print("I have no favourite ice cream.")
